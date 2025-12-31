6fbd057414dc6e1b5cff9372938840928ffea8c1
Shivam Yadav <shivamy121314@gmail.com>
2025-12-31 10:01:17 +0530

    add chunk model

---
+++ backend/src/models/Chunk.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+const chunkSchema = new Schema({
+    useId : {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true,
+    },
+    parentType:{
+        type: String,
+        enum : ["content","page"],
+        required : true,
+        index: true,
+    },
+    parentId : {
+        type : Schema.Types.ObjectId,
+        required : true,
+        index: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+
+    embedding: {
+      type: [Number],
+      required: true,
+    },
+
+    index: {
+      type: Number,
+      required: true,
+    },
+},{timestamps : {createdAt : true, updatedAt : true}})
+
+/**
+ * Composite indexes for fast RAG queries
+ */
+chunkSchema.index({ userId: 1, parentType: 1, parentId: 1 });
+chunkSchema.index({ userId: 1 });
+
+export const Chunk = mongoose.model("chunk",chunkSchema)
