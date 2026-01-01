79cfe4405f302902701ba38ecf824b5d4f56995c
Shivam Yadav <shivamy121314@gmail.com>
2026-01-01 09:24:33 +0530

    connecting mongo to altlas for session => does not work in localhost

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
+import "dotenv/config"
+
+const mongo_url = process.env.MONGO_DB_URL;
+
+if(!mongo_url){
+    throw new Error("MONGO DB URL required")
+}
 
 export const connectDB = async () => {
     try {
-        const connectionInstance  = await mongoose.connect("mongodb://localhost:27017/brainDead");
+        const connectionInstance  = await mongoose.connect(mongo_url);
         console.log(`\n MongoDB connected !! DB host: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MONGODB connection Failed",error);
