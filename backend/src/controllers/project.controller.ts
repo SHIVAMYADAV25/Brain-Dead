a39df2078fb37b132120c3ee03b33d0a2ef414c5
Shivam Yadav <shivamy121314@gmail.com>
2026-01-01 21:46:28 +0530

    access project using ID => controller

--- backend/src/controllers/project.controller.ts
+++ backend/src/controllers/project.controller.ts
@@ -93,5 +93,100 @@ export const projectController = {
         }
     },
 
+    // /projects => get
+
+    async getProject(req:Request,res:Response){
+        try {
+            const userId = req.userId;
     
+            if (!userId) {
+                return res.status(401).json({ success: false, error: "Unauthorized" });
+            }
+
+            const projects = await Project.find({userId}).select("_id name contentIds pageIds");
+
+            if(!projects){
+                return res.status(403).json({
+                        success:false,
+                        error : "error creating the Project"
+                    })
+            }
+
+            const response = projectListResponseSchema.parse(
+                projects.map(p=>({
+                    _id : p._id.toString(),
+                    name : p.name,
+                    contentIds : p.contentIds.toString(),
+                    pageIds : p.pageIds.toString()
+                }))
+            )
+
+            res.status(200).json({
+                success:true,
+                data : response
+            })
+
+        }catch(error){
+            console.error("getting project error:", error);
+            return res.status(401).json({
+                success : false,
+                "error" : error
+            })
+        }
+    },
+
+    // /projects/:Id => get
+
+    async getProjectById(req:Request,res:Response){
+        const parsed = IdParamSchema.safeParse(req.params)
+
+        if(!parsed.success){
+            return res.status(400).json({
+                    success: false,
+                    error: parsed.error.format(),
+                });
+        }
+
+       try {
+         const { id } = parsed.data;
+         const userId = req.userId;
+     
+         if (!userId) {
+             return res.status(401).json({ success: false, error: "Unauthorized" });
+         }
+ 
+         const project = await Project.findOne({
+             _id : id,
+             userId
+         })
+ 
+         if(!project){
+             return res.status(404).json({
+                 success: false,
+                 error: "Project not found"
+             })
+         }
+ 
+         const response = projectResponseSchema.parse({
+            _id : project._id.toString(),
+            name: project.name,
+            contentIds: project.contentIds,
+            pageIds: project.pageIds
+         })
+ 
+         res.status(200).json({
+             success: true,
+             data : response
+         })
+       } catch (error) {
+        console.error("getting project error:", error);
+            return res.status(401).json({
+                success : false,
+                "error" : "error on server side"
+            })
+       }
+    },
+
+    
+
 }
