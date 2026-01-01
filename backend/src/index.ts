c8275eb6a4b385d863ebb3f359eb74759afac962
Shivam Yadav <shivamy121314@gmail.com>
2026-01-01 21:48:00 +0530

    project middleware and route setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,9 +11,11 @@ import AuthRouter from "./routes/auth.route.js"
 import ContentRouter from "./routes/content.route.js";
 import { userMiddleware } from './middlewares/auth.middleware.js';
 import PageRouter from "./routes/page.route.js"
+import projectRouter from "./routes/project.route.js";
 
 app.use("/api/v1/user",AuthRouter);
 app.use("/api/v1/content",userMiddleware,ContentRouter);
 app.use("/api/v1/page",userMiddleware,PageRouter);
+app.use("/api/v1/project",userMiddleware,projectRouter);
 
 export default app
