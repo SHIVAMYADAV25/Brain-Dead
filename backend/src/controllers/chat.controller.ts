e7c38f642bc6dc830c79ac56307a7b7ae965933a
Shivam Yadav <shivamy121314@gmail.com>
2026-01-03 21:57:33 +0530

    chat controller done

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -4,7 +4,11 @@ import type { Request,Response } from "express";
 import { Page } from "../models/Page.model.js";
 import { Content } from "../models/Content.model.js";
 import { ChatContext } from "../models/ChatContext.model.js";
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
+import { Chat } from "../models/Chat.model.js";
+import { createEmbedding } from "../services/embedding.service.js";
+import { similaritySerach } from "../services/RAG/similarity.service.js";
+import { RAGLLM } from "../services/RAG/RAGLLM.service.js";
 
 const chatContextInputSchema = z.object({
   scope: z.enum(["page", "content"]),
@@ -79,7 +83,7 @@ const ChatController = {
         token,
         userId,
         scope,
-        scopeId : objectIds,
+        scopeIds : objectIds,
         expiresAt : new Date(Date.now() + 30 * 60 *1000)
       })
       
@@ -100,5 +104,139 @@ const ChatController = {
   // /chat
   async chat(req:Request,res:Response){
 
+    const parsed = chatInputSchema.safeParse(req.body);
+
+    if(!parsed.success){
+      return res.status(400).json({
+                    success: false,
+                    error: parsed.error.format(),
+                });
+    }
+
+    const {message,contextToken,chatId} = parsed.data;
+
+    const userId = req.userId;
+
+    if (!userId) {
+                  return res.status(401).json({ success: false, error: "Unauthorized" });
+              }
+
+    let scope:"page" | "content" ;
+    let scopeIds:Types.ObjectId[];
+    let chat;
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      // MODE 1 - first Message
+      if(contextToken){
+        const chatcontext = await ChatContext.findOne({
+          token : contextToken,
+          userId,
+          expiresAt : {$gt : new Date()}  //a comparison query operator meaning "greater than" (>) that selects documents where a field's value is numerically or lexicographically larger than the specified value
+        }).session(session);
+  
+        if(!chatcontext){
+          await session.abortTransaction();
+          return res.status(400).json({
+            success : false,
+            error : "context is expired"
+          })
+        }
+  
+        scope = chatcontext.scope; 
+        scopeIds = chatcontext.scopeIds;
+        
+  
+        const queryEmbedding = await createEmbedding(message);
+  
+        const context = await similaritySerach({queryEmbedding:queryEmbedding,userId: userId,parentType : scope ,parentIds : scopeIds})
+  
+        const answer = await RAGLLM(context,message);
+  
+        const chatDoc = new Chat({
+          userId,
+          scope,
+          scopeIds,
+          messages: [
+            { role: "user", text: message },
+            { role: "ai", text: answer }
+          ],
+          lastMessageAt: new Date()
+        });
+
+        await chatDoc.save({ session });
+
+        await ChatContext.deleteOne(
+          { token: contextToken, userId },
+          { session }
+        );
+
+        await session.commitTransaction();
+
+        return res.json({
+          success: true,
+          data: { chatId: chatDoc._id, answer }
+        });
+
+      } 
+  
+      /* MODE 2 — CONTINUATION */
+    
+      chat = await Chat.findOne({
+        _id:chatId,
+        userId
+      }).session(session);
+  
+      if (!chat) {
+        await session.abortTransaction();
+        return res.status(404).json({ success: false, error: "Chat not found" });
+      }
+  
+      //@ts-ignores
+      scope = chat.scope;
+      scopeIds = chat.scopeIds;
+  
+      // message ke embedding
+  
+      const queryEmbedding = await createEmbedding(message)
+  
+      // similarity search
+  
+      const context = await similaritySerach({queryEmbedding,userId,parentType:scope,parentIds :scopeIds})
+  
+      // give text store with embedding to llm
+      
+      // llm out 
+      const output_LLM = await RAGLLM(context,message);
+  
+      // save the llm out 
+  
+      chat.messages.push(
+        { role: "user", text: message },
+        { role: "ai", text: output_LLM }
+      )
+      chat.lastMessageAt = new Date();
+  
+      await chat.save({ session });
+  
+      await session.commitTransaction();
+      // send the response
+  
+      return res.status(200).json({
+        success : true,
+        data: {answer : output_LLM}
+      })
+    } catch (error) {
+      await session.abortTransaction();
+      console.error("Edit page failed:", error);
+
+      res.status(500).json({
+          success: false,
+          error: "Failed to update page"
+      });
+
+    }finally{
+        session.endSession();
+    }
   }
 }
